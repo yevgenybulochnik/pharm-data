9f20ca0b25b3ed8cd68edcc6881cf13165e0f611
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2021-04-11 17:48:59 -0500

    Base dir structure setup

---
+++ packages/rx/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='rx',
+    version='1.0.0',
+    package_dir={'':'src'},
+    packages=find_packages(where='src'),
+)
---
+++ packages/rx/tests/test_first_test.py
@@ -0,0 +1,6 @@
+from rx.core import hello_world
+
+
+def test_hello_world():
+    hello_world()
+    assert False
